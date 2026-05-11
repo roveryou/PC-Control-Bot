c11dd97718567fcb3f39bf29e63e0261f0e79aae
roveryou <roveryou8@gmail.com>
2026-05-11 20:44:30 +0200

    Change
    Updated BOT_TOKEN to a new value for security.

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ OWNER_ID =1481689477851385959  # ← HIER DEINE DISCORD-ID EINTRAGEN
 
 # Bot-Token (aus dem Discord Developer Portal)
 # WICHTIG: Erstelle einen NEUEN Token, der alte ist kompromittiert!
-BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.Ga9TEv.oBvWAS9oPs2BCumpF76vjkCTox_PCXhHveElfg"
+BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.GRQODA.JbTk6G4U-GMd5GA_AiqrVqIGUzYkqeSPX7tqW0"
