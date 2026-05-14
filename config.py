528c80fd2b3c45a23a5aab52ea72c12d50b1b475
roveryou <roveryou8@gmail.com>
2026-05-14 16:43:46 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Deine Discord User ID (nur DU hast Zugriff)
-OWNER_ID =1481689477851385959  # ← HIER DEINE DISCORD-ID EINTRAGEN
-
-# Bot-Token (aus dem Discord Developer Portal)
-# WICHTIG: Erstelle einen NEUEN Token, der alte ist kompromittiert!
-BOT_TOKEN = "MTUwMzQ2Mjg0MDIxNDc1MzM3MQ.Ga9TEv.oBvWAS9oPs2BCumpF76vjkCTox_PCXhHveElfg"
